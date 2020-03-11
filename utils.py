d6f855c778685e8a38993cc2daf0388a6868c95d
cassandrelepercque <cassandre.lepercque@etu.umontpellier.fr>
2020-03-11 23:29:57 +0100

    ajout d'une fonction de matrice aleatoire

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.animation as animation
 
 def afficher_iterations(function, matrix):
     """Affiche les étapes du je de la vie sur 10 itérations."""
@@ -14,3 +15,21 @@ def afficher_iterations(function, matrix):
         matrice_iteration = function(matrix)
         plt.imshow(matrice_iteration) #affiche les figures.
         plt.title("Itération" + str(i-1)) #titre de chaque figure.
+
+
+def video(matrix, jeu_np, iterations):
+    case = plt.matshow(matrix)
+
+    def animate(i):
+        case.set_array(jeu_np(np.array(matrix), i))
+        return(case)
+    anim = animation.FuncAnimation(plt.figure(), animate, frames = iterations)
+    return(anim)
+
+def matrix_alea(graine, matrix):
+    prop_active = (1 + graine) * 10 / 100
+    for i in range(1, np.shape(matrix)[0]-1):
+        for j in range(1, np.shape(matrix)[1]-1):
+            aleatoire = np.random.binomial(n=1, p=float(prop_active))
+            matrix[i, j] = aleatoire
+    return(matrix)
